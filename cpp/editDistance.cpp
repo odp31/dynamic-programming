dfe3f6dcb5ef158dd112d6b2d060c8c02d84edb8
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-25 13:27:08 -0500

    Create editDistance.cpp

---
+++ cpp/editDistance.cpp
@@ -0,0 +1,36 @@
+// AKA Levenshtein distance
+// measures min num of edits required to transform one string to another 
+// edits can be insertions, deletions or substitutions 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int editDistance(string str1, string str2, int m, int n)
+{
+  int dp[m+1][n+1];
+  // fill DP table
+  for(int i = 0; i <= m; i++){
+    for(int j = 0; j <= n; j++) {
+      if (i == 0)
+        dp[i][j] = j;
+      else if (j == 0)
+        dp[i][j] = i;
+      else if (str1[i-1] == str2[j-1])
+        dp[i][j] = dp[i - 1][j-1];
+      else
+        dp[i][j] = 1 + min(min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
+    }
+  }
+reutrn dp[m][n];
+}
+
+int main()
+{
+  string str1 = "sunday";
+  string str2 = "saturday";
+
+  cout << editDistance(str1, str2, str1.length(), str2.length()) << end1;
+  return 0;
+}
